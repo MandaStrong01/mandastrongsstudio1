78e9e68d64dd27cfff03a6fac344ff7999a6ce9a
MandaStrong01 <woolleya129@gmail.com>
2025-12-21 07:24:03 -0500

    Updated main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -3,6 +3,8 @@ import { createRoot } from 'react-dom/client';
 import App from './App.tsx';
 import './index.css';
 import { AuthProvider } from './contexts/AuthContext';
+import { HashRouter } from "react-router-dom";
+
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
