4480b48e4817c5a3a22a87840195a86c4c49e099
MandaStrong01 <woolleya129@gmail.com>
2025-12-22 00:42:42 -0500

    Update index.tsx

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <div style={{ color: 'white', background: 'black', minHeight: '100vh' }}>
+      MandaStrong Studio – Build OK
+    </div>
   </React.StrictMode>
 );
