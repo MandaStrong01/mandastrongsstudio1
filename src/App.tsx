10c642df121316b9d30ca9eba3bc20380b33c7ac
MandaStrong01 <woolleya129@gmail.com>
2026-01-05 01:55:42 -0500

    Updated README.md

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,13 @@
-import { HashRouter, Routes, Route } from "react-router-dom";
-
-import Page1 from "./pages/Page1";
-import MediaLibrary from "./pages/MediaLibrary";
-import Page11 from "./pages/Page11";
+import { useState } from 'react';
+import Landing from './components/Landing';
+import Studio from './components/Studio';
 
 export default function App() {
-  return (
-    <HashRouter>
-      <Routes>
-        <Route path="/" element={<Page1 />} />
-        <Route path="/media" element={<MediaLibrary />} />
-        <Route path="/editor" element={<Page11 />} />
-      </Routes>
-    </HashRouter>
-  );
+  const [showStudio, setShowStudio] = useState(false);
+
+  if (showStudio) {
+    return <Studio />;
+  }
+
+  return <Landing onGetStarted={() => setShowStudio(true)} />;
 }
